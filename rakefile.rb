ad53deb5c5b2f8f52f107f7baa53c10e9916a823
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-03-24 22:22:00 +1000

    - and here we go

---
+++ rakefile.rb
@@ -0,0 +1,22 @@
+require 'rake'
+require 'albacore'
+
+DOT_NET_PATH = "#{ENV["SystemRoot"]}\\Microsoft.NET\\Framework\\v4.0.30319"
+
+ENV["config"] = "Debug" if ENV["config"].nil?
+CONFIG = ENV["config"]
+
+task :default => [:compile, :test]
+
+desc "Compile solution"
+msbuild :compile do |msb|
+    msb.properties :configuration => CONFIG
+    msb.path_to_command = File.join(DOT_NET_PATH, "msbuild.exe")
+    msb.targets :Clean, :Build
+    msb.solution = "Norman.sln"
+end
+
+xunit :test => [:compile] do |xunit|
+    xunit.path_to_command = "lib/xunit.console.clr4.exe"
+    xunit.assembly = "tst/Norman.Tests/bin/#{CONFIG}/Norman.Tests.dll"
+end
---
+++ readme.markdown
@@ -0,0 +1,5 @@
+Joey
+========
+
+### In few words
+Joey is a _scenario driven_ integration testing framework built on top of xUnit.NET
